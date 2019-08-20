69d328c38f631a822b159bfb4065ac97d842ac36
gballmer <gballmer@teamcoact.com>
2019-08-20 17:27:08 -0400

    update template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,38 @@
-const path = require(`path`)
+// const path = require(`path`)
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions
 
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `)
+//   const result = await graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
+//   // Handle errors
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.path,
-      component: path.resolve(
-        `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-      ),
-      context: {}, // additional data can be passed via context
-    })
-  })
-}
+//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     createPage({
+//       path: node.frontmatter.path,
+//       component: path.resolve(
+//         `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+//       ),
+//       context: {}, // additional data can be passed via context
+//     })
+//   })
+// }
