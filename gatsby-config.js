719b88f553ee10ab54a6db5487e4cfd109f44f54
gballmer <gballmer@teamcoact.com>
2019-08-20 16:29:36 -0400

    update config.yml and gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,22 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/img`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
